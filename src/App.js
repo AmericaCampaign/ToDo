bba106fbf2c169fc8e849c5b996f2bd4072d3b13
fresh5447 <doug@kosmojo.com>
2017-11-17 15:11:45 -0700

    application fully functional

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Header from './Header'
+import ToDoContainer from './ToDoContainer'
+
+const styles = {
+  container: {
+    width: '100%'
+  }
+}
+
 
 const App = () =>
-  <div>
-    <h3>Hello World</h3>
+  <div style={styles.container}>
+    <Header />
+    <ToDoContainer />
   </div>
 
 export default App
